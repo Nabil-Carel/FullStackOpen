546d3aa6d7cda99fd8ac9dc5590bb8491ce53e45
Nabil-Carel <nabilgbaguidi8@gmail.com>
2020-05-31 16:15:30 -0400

    useless folder removed

--- Part2/Exercises 2.15-2.18/src/components/SubmitButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SubmitButton = ({ handleSubmit, text }) => (
-  <button type="submit" onClick={handleSubmit}>
-    {text}
-  </button>
-);
-
-export default SubmitButton;
